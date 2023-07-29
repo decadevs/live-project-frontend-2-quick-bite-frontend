372f023c98ffcf0f08d23cb9afbe2d09b220d5c1
Decagon <decagon@Decagons-MacBook-Air.local>
2023-07-29 20:03:46 +0100

    added userLogin and signUp form

---
+++ client/src/components/userLogin.tsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const LoginForm = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+
+    const handleLogin = () => {
+        // Perform authentication logic here and dispatch actions if needed
+        navigate('/otp');
+    };
+
+    return (
+        <div className="flex justify-center items-center h-screen bg-edf0eb"> 
+            <div className="w-1/3 p-8 bg-white rounded-xl shadow">
+                <h2 className="text-2xl font-bold mb-4">User Login</h2>
+                <div className="mb-4">
+                    <label htmlFor="email" className="block font-medium">
+                        Email
+                    </label>
+                    <input
+                        type="text"
+                        id="email"
+                        className="w-full p-2 border rounded"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <div className="mb-4">
+                    <label htmlFor="password" className="block font-medium">
+                        Password
+                    </label>
+                    <input
+                        type="password"
+                        id="password"
+                        className="w-full p-2 border rounded"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <button
+                    className="w-full p-2 bg-deepBlue text-white rounded-xl"
+                    onClick={handleLogin}
+                >
+                    Login
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default LoginForm;
