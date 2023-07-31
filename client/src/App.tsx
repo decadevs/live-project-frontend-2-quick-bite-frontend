144ce721d2c5eb85ff921dd44f24abb55f97100e
peak2 <36886305+peak2@users.noreply.github.com>
2023-07-31 22:30:18 +0100

    yes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,19 +2,15 @@ import Home from "./pages/Home/Home"
 import { Routes, Route } from 'react-router-dom'
 import SignUp from "./pages/Auth/signUp"
 import SignIn from "./pages/Auth/signIn"
-<<<<<<< HEAD
+// import OtpVerification from "./components/otpVerification/OtpVerification"
 import FoodPage from "./pages/FoodPage"
 import SingleVendor from "./pages/SingleVendor"
-=======
-import OtpVerification from "./components/otpVerification/OtpVerification"
 
->>>>>>> d94354bd2c7720f50894d926b9527a26c21479db
 function App() {
 
 
   return (
     <>
-<<<<<<< HEAD
      <main>
       <Routes>
       <Route path='/' element = {<Home/>}></Route>
@@ -22,19 +18,11 @@ function App() {
       <Route path='login' element = {<SignIn/>}></Route>     
       <Route path='/register' element = {<SignUp/>}></Route>
       <Route path='/food' element = {<FoodPage/>}></Route>
+      {/* <Route path='/otp' element={<OtpVerification />}></Route> */}
            
      </Routes>
      </main>
-=======
-      <main>
-        <Routes>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='login' element={<SignIn />}></Route>
-          <Route path='/register' element={<SignUp />}></Route>
-          <Route path='/otp' element={<OtpVerification />}></Route>
-        </Routes>
-      </main>
->>>>>>> d94354bd2c7720f50894d926b9527a26c21479db
+
 
     </>
   )
