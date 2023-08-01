fe6f9fdc70438db1274f392ed91fd8bfc75c916c
Lawrence ime <lawrenceime246@gmail.com>
2023-08-01 18:07:37 +0100

    Second change made on the vendor's page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,8 @@ import Home from "./pages/Home/Home"
 import { Routes, Route } from 'react-router-dom'
 import SignUp from "./pages/Auth/signUp"
 import SignIn from "./pages/Auth/signIn"
-import OtpVerification from "./components/otpVerification/OtpVerification"
+// import OtpVerification from "./components/otpVerification/OtpVerification"
+import VendorsPage from "./pages/VendorsPage"
 
 function App() {
 
@@ -14,7 +15,8 @@ function App() {
           <Route path='/' element={<Home />}></Route>
           <Route path='login' element={<SignIn />}></Route>
           <Route path='/register' element={<SignUp />}></Route>
-          <Route path='/otp' element={<OtpVerification />}></Route>
+          {/* <Route path='/otp' element={<OtpVerification />}></Route> */}
+          <Route path='/vendors' element={<VendorsPage/>}></Route>
         </Routes>
       </main>
 
---
+++ client/src/components/Cards.tsx
@@ -0,0 +1,181 @@
+import './Cards.css'
+import Image from '../assets/Pulled Pork.jpeg'
+
+const Cards:React.FC = () => {
+    return (
+      <div className='wrapper'>
+           <div className='card-container'>
+        <div className="image-container">
+              <img src= {Image} alt="" />
+        </div>
+        <div className='card-content'>
+        <div className="card-title">
+          <h3>Vendors food</h3>
+        </div>
+        <div className="card-body">
+          <p>tedshjjkzkz,µ</p>
+        </div>
+        <div className="btn">
+        </div>
+        
+          <button>
+              <a>
+               view more 
+              </a>
+          </button>
+        </div>
+      </div>
+      <div className='card-container'>
+        <div className="image-container">
+              <img src= {Image} alt="" />
+        </div>
+        <div className='card-content'>
+        <div className="card-title">
+          <h3>Vendors food</h3>
+        </div>
+        <div className="card-body">
+          <p>tedshjjkzkz,µ</p>
+        </div>
+        <div className="btn">
+        </div>
+        
+          <button>
+              <a>
+               view more 
+              </a>
+          </button>
+        </div>
+      </div>
+      <div className='card-container'>
+        <div className="image-container">
+              <img src= {Image} alt="" />
+        </div>
+        <div className='card-content'>
+        <div className="card-title">
+          <h3>Vendors food</h3>
+        </div>
+        <div className="card-body">
+          <p>tedshjjkzkz,µ</p>
+        </div>
+        <div className="btn">
+        </div>
+        
+          <button>
+              <a>
+               view more 
+              </a>
+          </button>
+        </div>
+      </div>
+      <div className='card-container'>
+        <div className="image-container">
+              <img src= {Image} alt="" />
+        </div>
+        <div className='card-content'>
+        <div className="card-title">
+          <h3>Vendors food</h3>
+        </div>
+        <div className="card-body">
+          <p>tedshjjkzkz,µ</p>
+        </div>
+        <div className="btn">
+        </div>
+        
+          <button>
+              <a>
+               view more 
+              </a>
+          </button>
+        </div>
+      </div>
+      <div className='card-container'>
+        <div className="image-container">
+              <img src= {Image} alt="" />
+        </div>
+        <div className='card-content'>
+        <div className="card-title">
+          <h3>Vendors food</h3>
+        </div>
+        <div className="card-body">
+          <p>tedshjjkzkz,µ</p>
+        </div>
+        <div className="btn">
+        </div>
+        
+          <button>
+              <a>
+               view more 
+              </a>
+          </button>
+        </div>
+      </div>
+      <div className='card-container'>
+        <div className="image-container">
+              <img src= {Image} alt="" />
+        </div>
+        <div className='card-content'>
+        <div className="card-title">
+          <h3>Vendors food</h3>
+        </div>
+        <div className="card-body">
+          <p>tedshjjkzkz,µ</p>
+        </div>
+        <div className="btn">
+        </div>
+        
+          <button>
+              <a>
+               view more 
+              </a>
+          </button>
+        </div>
+      </div>
+      <div className='card-container'>
+        <div className="image-container">
+              <img src= {Image} alt="" />
+        </div>
+        <div className='card-content'>
+        <div className="card-title">
+          <h3>Vendors food</h3>
+        </div>
+        <div className="card-body">
+          <p>tedshjjkzkz,µ</p>
+        </div>
+        <div className="btn">
+        </div>
+        
+          <button>
+              <a>
+               view more 
+              </a>
+          </button>
+        </div>
+      </div>
+      <div className='card-container'>
+        <div className="image-container">
+              <img src= {Image} alt="" />
+        </div>
+        <div className='card-content'>
+        <div className="card-title">
+          <h3>Vendors food</h3>
+        </div>
+        <div className="card-body">
+          <p>tedshjjkzkz,µ</p>
+        </div>
+        <div className="btn">
+        </div>
+        
+          <button>
+              <a>
+               view more 
+              </a>
+          </button>
+        </div>
+        
+      </div>
+      </div>
+     
+    )
+  }
+  
+  export default Cards
