039601811467bf3675eca74b5942e2a9a548a347
Jidekas <132021739+Jidekas@users.noreply.github.com>
2023-08-11 14:52:09 +0100

    add all vendor's foods page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,44 +1,41 @@
-import Home from "./pages/Home/Home"
-import { Routes, Route } from 'react-router-dom'
-import SignUp from "./pages/Auth/signUp"
-import SignIn from "./pages/Auth/signIn"
-import FoodPage from "./pages/FoodPage"
-import SingleVendor from "./pages/SingleVendor"
-import VendorSignupForm from "./components/vendorSignUp"
-import VendorLoginForm from "./components/vendorLogin"
-import VendorRegNO from "./components/vendorVerifyForm"
-import VendorPassword from "./components/vendorPassword"
-import OtpVerificationPage from "./pages/OtpVerificationPage"
-import Footer from "./components/Footer"
-import VendorsPage from "./pages/VendorsPage"
+import Home from "./pages/Home/Home";
+import { Routes, Route } from "react-router-dom";
+import SignUp from "./pages/Auth/signUp";
+import SignIn from "./pages/Auth/signIn";
+import FoodPage from "./pages/FoodPage";
+import SingleVendor from "./pages/SingleVendor";
+import VendorSignupForm from "./components/vendorSignUp";
+import VendorLoginForm from "./components/vendorLogin";
+import VendorRegNO from "./components/vendorVerifyForm";
+import VendorPassword from "./components/vendorPassword";
+import OtpVerificationPage from "./pages/OtpVerificationPage";
+import Footer from "./components/Footer";
+import VendorsPage from "./pages/VendorsPage";
+import AllVendorFoods from "./pages/AllVendorFoods";
 
 function App() {
-
-
   return (
     <>
-     <main>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
 
-      <Routes>
-      <Route path='/' element = {<Home/>}></Route>
-    
-      <Route path='login' element = {<SignIn/>}></Route>     
-      <Route path='/register' element = {<SignUp/>}></Route>
-      <Route path='/vendor' element = {<VendorSignupForm/>}></Route>
-      <Route path='/vendorlogin' element = {<VendorLoginForm/>}></Route> 
-      <Route path='/verifyVendor' element = {<VendorRegNO/>}></Route>   
-      <Route path='/vendorPassword' element = {<VendorPassword/>}></Route> 
-      <Route path='/:id' element = {<SingleVendor/>}></Route> 
-      <Route path='/food' element = {<FoodPage/>}></Route> 
-      <Route path='/otp' element={<OtpVerificationPage />}></Route>
-      <Route path='/vendors' element={<VendorsPage />}></Route>
-     </Routes>
-     <Footer/>
-     </main>
-
-
+          <Route path="login" element={<SignIn />}></Route>
+          <Route path="/register" element={<SignUp />}></Route>
+          <Route path="/vendor" element={<VendorSignupForm />}></Route>
+          <Route path="/vendorlogin" element={<VendorLoginForm />}></Route>
+          <Route path="/verifyVendor" element={<VendorRegNO />}></Route>
+          <Route path="/vendorPassword" element={<VendorPassword />}></Route>
+          <Route path="/:id" element={<SingleVendor />}></Route>
+          <Route path="/food" element={<FoodPage />}></Route>
+          <Route path="/otp" element={<OtpVerificationPage />}></Route>
+          <Route path="/vendors" element={<VendorsPage />}></Route>
+          <Route path="/allvendorfoods" element={<AllVendorFoods />}></Route>
+        </Routes>
+        <Footer />
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
