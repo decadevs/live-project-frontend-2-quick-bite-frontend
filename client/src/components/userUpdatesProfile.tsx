3f7b0e00db6adbdfe444f656c4f6d73e96314a11
Kingsley Ibe <kingsley.ibe@decagon.dev>
2023-08-13 12:50:33 +0100

    Resolved merged conflicts

--- client/src/components/userUpdatesProfile.tsx
+++ client/src/components/userUpdatesProfile.tsx
@@ -1,15 +1,29 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { useState, ChangeEvent, FormEvent } from 'react';
+import { useState, useEffect, ChangeEvent, FormEvent } from 'react';
+import { useAppDispatch, useAppSelector } from '../store/hooks';
+import { updateUserProfile } from '../slices/authSlice';
+import { showErrorToast } from '../utility/toast';
 
-const UserUpdatesProfile = () => {
-    const [user, setUser] = useState({
-        firstName: '',
+const initialUserData = {firstName: '',
         lastName: '',
         email: '',
         phoneNumber: '',
-        address: ''
-         
-    });
+        address: ""} 
+
+
+ const UserUpdatesProfile = () => {
+    const [user, setUser] = useState(initialUserData)
+
+    const dispatch = useAppDispatch()
+       
+     const {user:logedInUser} = useAppSelector((state) => state.auth)
+     // eslint-disable-next-line @typescript-eslint/no-unused-vars
+     const [loading, setLoading] = useState(false)
+
+     useEffect(()=>{
+        setUser({...user, firstName:logedInUser?.firstname as string, lastName:logedInUser?.lastname as string, email:logedInUser?.email as string, address:logedInUser?.address as string, phoneNumber:logedInUser?.phone_no as string})
+     }, [logedInUser, user])
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
@@ -24,8 +38,28 @@ const UserUpdatesProfile = () => {
  
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    function handleSubmit(_event: FormEvent<HTMLFormElement>): void {
-        throw new Error('Function not implemented.');
+    async function handleSubmit (event: FormEvent<HTMLFormElement>): Promise<void> {
+    try{
+        event.preventDefault()
+        const payload = {...user};
+
+        const data = await dispatch(updateUserProfile(payload)).unwrap()
+        
+        console.log(data.message)
+
+        setUser(initialUserData)
+       // throw new Error('Function not implemented.');
+    }catch (error:any) {
+        setLoading(false);
+        if (error.response) {
+            showErrorToast(error.response.data.message);
+        } else if (error.request) {
+            showErrorToast("Internal Server Error");
+        } else {
+            showErrorToast(`Error, ${error.message}`);
+        }
+    }
+        
     }
 
     
@@ -39,7 +73,7 @@ const UserUpdatesProfile = () => {
                         <input
                             type="text"
                             placeholder="First Name"
-                            name="firstNameOfUser"
+                            name="firstName"
                             value={user.firstName}
                             onChange={handleChange}
                             className="w-full p-2 border border-gray-300 rounded mb-4"
@@ -48,7 +82,7 @@ const UserUpdatesProfile = () => {
                         <input
                             type="text"
                             placeholder="last Name"
-                            name="lastNameOfUser"
+                            name="lastName"
                             value={user.lastName}
                             onChange={handleChange}
                             className="w-full p-2 border border-gray-300 rounded mb-4"
@@ -91,7 +125,7 @@ const UserUpdatesProfile = () => {
                             Save 
                         </button>
                         <button
-                            type="submit"
+                            type="button"
                             className="w-1/3 p-2 bg-deepBlue text-white rounded">
                             Cancel 
                         </button>
