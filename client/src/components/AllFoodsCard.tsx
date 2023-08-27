c7206dc39a852266e4717936265e772774a2a7f4
Decagon <decagon@MACs-MacBook-Air.local>
2023-08-27 20:08:16 +0100

    get new popular and all foods page

--- client/src/components/AllFoodsCard.tsx
+++ client/src/components/AllFoodsCard.tsx
@@ -1,14 +1,28 @@
-import CardsFood from './CardsFood'
+
+import { useAppDispatch, useAppSelector } from '../store/hooks';
+import { useEffect } from "react";
+import { getUserAllFoods } from '../slices/userGetAllFoodSlice';
 import './Cards.css'
-import Image from "../assets/4.food.jpg"
+import { Link } from "react-router-dom";
+import Image1 from "../assets/all-foods.jpg"
+
 
 
 const AllFoodsCard = () => {
 
+  const dispatch = useAppDispatch();
+  const {allFoods, isLoading} = useAppSelector((state) => state.userAllFood)
+
+  console.log(allFoods, isLoading)
+
+useEffect(()=> {
+  dispatch(getUserAllFoods())
+}, [dispatch])
+
   const titleStyle = {
-    backgroundImage: `url(${Image})`,
-    backgroundSize: 'cover', // Adjust as needed
-    backgroundPosition: 'cover', // Adjust as needed
+    backgroundImage: `url(${Image1})`,
+    backgroundSize: 'cover', 
+    backgroundPosition: 'cover', 
     width: '100%',
     height: '420px',
     
@@ -18,9 +32,34 @@ const AllFoodsCard = () => {
     return (
       <div>
         <div className="title-AllFoods" style={titleStyle}>
-        <h1>All Foods</h1>
+        <h1 className='all-foods'>All Foods</h1>
       </div>
-        <CardsFood />
+      <div className="wrapper">
+
+{allFoods?.map((item, index) => (
+
+<div className="card-container">
+    <div className="image-container">
+        <img src={item.food_image} alt="" />
+    </div>
+    <div className="card-content" key={index}>
+        <div className="card-title">
+            <h3 style={{ fontSize: '1.5rem', color: 'green', fontWeight: 'bold' }}>{item.name}</h3>
+        </div>
+        <div className="card-body">
+            <p>{item.description}</p>
+        </div>
+        <div className="btn"></div>
+
+        <Link to="/allvendorfoods">
+            <button>
+                <a className="view">view more</a>
+            </button>
+        </Link>
+    </div>
+</div> ))}
+
+</div>
       </div>
 
 
--- client/src/components/PopularFoodsCard.tsx
+++ client/src/components/PopularFoodsCard.tsx
@@ -1,27 +1,63 @@
-import CardsFood from './CardsFood';
-import './Cards.css'
-import Image from "../assets/1food.jpg"
-
-
+import { Link } from "react-router-dom";
+import './Cards.css';
+import Image1 from "../assets/1food.jpg";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { getPopularFood } from "../slices/popularSlice";
+import { useEffect } from "react";
 const PopularFoodsCard = () => {
 
+  const dispatch = useAppDispatch();
+
+  const {popularFood, isLoading} = useAppSelector((state) => state.popularFood)
+
+  console.log(isLoading, popularFood)
+  useEffect(() => {
+      dispatch(getPopularFood())
+  }, [dispatch])
+
+
   const titleStyle = {
-    backgroundImage: `url(${Image})`,
-    backgroundSize: 'cover', // Adjust as needed
-    backgroundPosition: 'center', // Adjust as needed
+    backgroundImage: `url(${Image1})`,
+    backgroundSize: 'cover', 
+    backgroundPosition: 'center', 
     width: '100%',
     height: '420px'
   };
+
+  //const Image2 = popularFood.map((item) => item.food_image)
+  // const VendorName = popularFood.map((item) => item.name)
+  // const description = popularFood.map((item) => item.description)
     return (
       <div>
         <div className="title-popularfoods" style={titleStyle}>
-        <h1>Popular Foods</h1>
+        <h1 className='popular-foods' >Popular Foods</h1>
       </div>
-      <CardsFood />
+      <div className="wrapper">
+      {popularFood?.map((item, index) => (
+      
+            <div className="card-container" key={index}>
+                <div className="image-container">
+                    <img src={item.food_image} alt="" />
+                </div>
+                <div className="card-content">
+                    <div className="card-title">
+                        <h3 style={{ fontSize: '1.5rem', color: 'green', fontWeight: 'bold' }}>{item.name}</h3>
+                    </div>
+                    <div className="card-body">
+                        <p>{item.description}</p>
+                    </div>
+                    <div className="btn"></div>
+
+                    <Link to="/allvendorfoods">
+                        <button>
+                            <a className="view">view more</a>
+                        </button>
+                    </Link>
+                </div>
+            </div>))}
+            </div>
     </div>
   )
 }
 
 export default PopularFoodsCard;
-
-
